8b54e3ff4fdc1f9ccb4c1ed33201708849320372
yana.radeva7 <yana.radeva7@gmail.com>
2022-10-25 09:55:18 +0300

    create implementations of repositories for deletable and non-deletable entities

---
+++ ExploreBulgaria.Data/Repositories/EfDeletableEntityRepository.cs
@@ -0,0 +1,40 @@
+﻿using ExploreBulgaria.Data.Common.Models;
+using ExploreBulgaria.Data.Common.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExploreBulgaria.Data.Repositories
+{
+    public class EfDeletableEntityRepository<TEntity> : EfRepository<TEntity>, IDeletableEnityRepository<TEntity>
+        where TEntity : class, IDeletableEntity
+    {
+        public EfDeletableEntityRepository(ApplicationDbContext context)
+            : base(context)
+        {
+        }
+
+        public override IQueryable<TEntity> All() => base.All().Where(x => !x.IsDeleted);
+
+        public override IQueryable<TEntity> AllAsNoTracking() =>base.AllAsNoTracking().Where(x => !x.IsDeleted);
+
+        public IQueryable<TEntity> AllWithDeleted() => base.All().IgnoreQueryFilters();
+
+        public IQueryable<TEntity> AllAsNoTrackingWithDeleted() => base.AllAsNoTracking().IgnoreQueryFilters();
+
+        public void HardDelete(TEntity entity) => base.Delete(entity);
+
+        public void Undelete(TEntity entity)
+        {
+            entity.IsDeleted = false;
+            entity.DeletedOn = null;
+            base.Update(entity);
+        }
+
+        public override void Delete(TEntity entity)
+        {
+            entity.IsDeleted = true;
+            entity.DeletedOn = DateTime.UtcNow;
+            base.Update(entity);
+        }
+    }
+
+}
