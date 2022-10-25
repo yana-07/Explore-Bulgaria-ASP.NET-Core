b33c13a7f26ae9ee913214830295483a1eee103f
yana.radeva7 <yana.radeva7@gmail.com>
2022-10-25 09:53:55 +0300

    create repositories for deletable and non-deletable entities

---
+++ ExploreBulgaria.Data.Common/Repositories/IDeletableEnityRepository.cs
@@ -0,0 +1,16 @@
+﻿using ExploreBulgaria.Data.Common.Models;
+
+namespace ExploreBulgaria.Data.Common.Repositories
+{
+    public interface IDeletableEnityRepository<TEntity> : IRepository<TEntity>
+        where TEntity : class, IDeletableEntity
+    {
+        IQueryable<TEntity> AllWithDeleted();
+
+        IQueryable<TEntity> AllAsNoTrackingWithDeleted();
+
+        void HardDelete(TEntity entity);
+
+        void Undelete(TEntity entity);
+    }
+}
