4e32a4f2d1bb207b0aa739feeecc2b7728b37863
yana.radeva7 <yana.radeva7@gmail.com>
2022-10-25 10:17:39 +0300

    extend default identity user

---
+++ ExploreBulgaria.Data.Models/ApplicationUser.cs
@@ -0,0 +1,30 @@
+﻿using ExploreBulgaria.Data.Common.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace ExploreBulgaria.Data.Models
+{
+    public class ApplicationUser : IdentityUser, IDeletableEntity, IAuditInfo
+    {
+        public ApplicationUser()
+        {
+            this.Id = Guid.NewGuid().ToString();
+            this.Roles = new HashSet<IdentityUserRole<string>>();
+            this.Claims = new HashSet<IdentityUserClaim<string>>();
+            this.Logins = new HashSet<IdentityUserLogin<string>>();
+        }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+
+        public ICollection<IdentityUserRole<string>> Roles { get; set; }
+
+        public ICollection<IdentityUserClaim<string>> Claims { get; set; }
+
+        public ICollection<IdentityUserLogin<string>> Logins { get; set; }
+    }
+}
