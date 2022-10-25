0b63d1421b2f17841b47dbc3bd61f8e80da7d66e
yana.radeva7 <yana.radeva7@gmail.com>
2022-10-25 15:34:46 +0300

    add entity models constants

---
+++ ExploreBulgaria.Data.Models/Constants/DataConstants.cs
@@ -0,0 +1,31 @@
+﻿namespace ExploreBulgaria.Data.Models.Constants
+{
+    public class DataConstants
+    {
+        public class Attraction
+        {
+            public const int NameMaxLength = 50;
+            public const int DescriptionMaxLength = 200;
+        }
+
+        public class Category
+        {
+            public const int NameMaxLength = 50;
+        }
+
+        public class Subcategory
+        {
+            public const int NameMaxLength = 50;
+        }
+
+        public class Region
+        {
+            public const int NameMaxLength = 50;
+        }
+
+        public class Comment
+        {
+            public const int TextMaxLength = 300;
+        }
+    }
+}
