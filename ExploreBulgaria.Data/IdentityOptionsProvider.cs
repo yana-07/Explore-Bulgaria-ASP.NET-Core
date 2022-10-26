dc1102d7b78a4cf072e1807c703054b01ce29d3e
yana.radeva7 <yana.radeva7@gmail.com>
2022-10-26 19:35:28 +0300

    add IdentityOptions to be used in the Program.cs

---
+++ ExploreBulgaria.Data/IdentityOptionsProvider.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExploreBulgaria.Data
+{
+    public static class IdentityOptionsProvider
+    {
+        public static void GetIdentityOptions(IdentityOptions options)
+        {
+            options.Password.RequireDigit = false;
+            options.Password.RequireLowercase = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequiredLength = 6;
+        }
+    }
+}
