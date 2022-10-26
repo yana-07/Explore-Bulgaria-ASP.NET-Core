6c05b5b08c7a2f010c822272860dd14f6f04a09c
yana.radeva7 <yana.radeva7@gmail.com>
2022-10-26 20:46:29 +0300

    update ApplicationDbContext to hold DbSets, set CreatedOn andModified onproperties of BaseModel entities, apply entity type configurations, set global query filter for deletable entities, create index on IsDeleted property and set delete behavior of navigation properties to restrict

--- ExploreBulgaria.Data/ApplicationDbContext.cs
+++ ExploreBulgaria.Data/ApplicationDbContext.cs
@@ -1,13 +1,112 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using ExploreBulgaria.Data.Common.Models;
+using ExploreBulgaria.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 
 namespace ExploreBulgaria.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
     {
+        private static readonly MethodInfo SetIsDeletedQueryFilterMethod =
+            typeof(ApplicationDbContext).GetMethod(
+                nameof(SetIsDeletableQueryFilter),
+                BindingFlags.NonPublic | BindingFlags.Static)!;
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        public DbSet<Attraction> Attractions { get; set; } = null!;
+
+        public DbSet<Category> Categories { get; set; } = null!;
+
+        public DbSet<Subcategory> Subcategories { get; set; } = null!;
+
+        public DbSet<Region> Regions { get; set; } = null!;
+
+        public DbSet<Image> Images { get; set; } = null!;
+
+        public DbSet<Comment> Comments { get; set; } = null!;
+
+        public DbSet<Vote> Votes { get; set; } = null!;
+
+        public override int SaveChanges() => this.SaveChanges(true);
+
+        public override int SaveChanges(bool acceptAllChangesOnSuccess)
+        {
+            this.ApplyAuditInfoRules();
+            return base.SaveChanges(acceptAllChangesOnSuccess);
+
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+            => this.SaveChangesAsync(true, cancellationToken);
+
+        public override Task<int> SaveChangesAsync(
+            bool acceptAllChangesOnSuccess,
+            CancellationToken cancellationToken = default)
+        {
+            this.ApplyAuditInfoRules();
+            return base.SaveChangesAsync(acceptAllChangesOnSuccess, cancellationToken);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            this.ApplyConfigurations(builder);
+
+            EntityIndexesConfiguration.Configure(builder);
+
+            var entityTypes = builder.Model.GetEntityTypes().ToList();
+
+            // Sets global filter for not deleted enitites only
+            var deletableEntityTypes = entityTypes
+                .Where(et => et.ClrType != null && typeof(IDeletableEntity).IsAssignableFrom(et.ClrType));
+            foreach (var deletableEntityType in deletableEntityTypes)
+            {
+                var method = SetIsDeletedQueryFilterMethod.MakeGenericMethod(deletableEntityType.ClrType);
+                method.Invoke(null, new object[] { builder });
+            }
+
+            // Disable cascade delete
+            var foreignKeys = entityTypes
+                .SelectMany(et => et.GetForeignKeys().Where(fk => fk.DeleteBehavior == DeleteBehavior.Cascade));
+            foreach (var foreignKey in foreignKeys)
+            {
+                foreignKey.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+        }
+
+        private static void SetIsDeletableQueryFilter<T>(ModelBuilder builder)
+            where T : class, IDeletableEntity
+            => builder.Entity<T>().HasQueryFilter(e => !e.IsDeleted);
+
+        // Applies configurations
+        private void ApplyConfigurations(ModelBuilder builder) 
+            => builder.ApplyConfigurationsFromAssembly(this.GetType().Assembly);
+
+        private void ApplyAuditInfoRules()
+        {
+            var changedEntries = this.ChangeTracker.Entries()
+                .Where(e =>
+                     e.Entity is IAuditInfo &&
+                    (e.State == EntityState.Added || e.State == EntityState.Modified));
+
+            foreach (var entry in changedEntries)
+            {
+                var entity = (IAuditInfo)entry.Entity;
+                if (entry.State == EntityState.Added && entity.CreatedOn == default)
+                {
+                    entity.CreatedOn = DateTime.UtcNow;
+                }
+                else
+                {
+                    entity.ModifiedOn = DateTime.UtcNow;
+                }
+            }
         }
     }
 }
