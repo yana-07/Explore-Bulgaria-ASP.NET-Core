ebe74b527660adee4b75180fc8b4b2f4715e30eb
yana.radeva7 <yana.radeva7@gmail.com>
2022-10-26 19:13:20 +0300

    configure mapping  tables' composite primary keys

---
+++ ExploreBulgaria.Data/Configurations/AttractionUserVisitedConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using ExploreBulgaria.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ExploreBulgaria.Data.Configurations
+{
+    public class AttractionUserVisitedConfiguration : IEntityTypeConfiguration<AttractionUserVisited>
+    {
+        public void Configure(EntityTypeBuilder<AttractionUserVisited> builder)
+            => builder.HasKey(x => new { x.AttractionId, x.UserId });
+    }
+}
---
+++ ExploreBulgaria.Data/Configurations/UserAddedToFavoritesConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using ExploreBulgaria.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace ExploreBulgaria.Data.Configurations
+{
+    public class UserAddedToFavoritesConfiguration : IEntityTypeConfiguration<AttractionUserAddedToFavorites>
+    {
+        public void Configure(EntityTypeBuilder<AttractionUserAddedToFavorites> builder)
+            => builder.HasKey(x => new { x.AttractionId, x.UserId });
+    }
+}
