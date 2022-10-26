e9804bd782b24eb0066ed21d3bd142aebebc80d9
yana.radeva7 <yana.radeva7@gmail.com>
2022-10-26 19:34:37 +0300

    configure deletable entities to have its IsDeleted properties as index

---
+++ ExploreBulgaria.Data/EntityIndexesConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using ExploreBulgaria.Data.Common.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExploreBulgaria.Data
+{
+    internal static class EntityIndexesConfiguration
+    {
+        public static void Configure(ModelBuilder builder)
+        {
+            // IDeletableEntity.IsDeletedindex
+            var deletableEntityTypes = builder.Model
+                .GetEntityTypes()
+                .Where(et => et.ClrType != null && typeof(IDeletableEntity).IsAssignableFrom(et.ClrType));
+            foreach (var deletableEntityType in deletableEntityTypes)
+            {
+                builder.Entity(deletableEntityType.ClrType).HasIndex(nameof(IDeletableEntity.IsDeleted));
+            }
+        }
+    }
+}
