3b71033ac5effe4c44544a69d7bf10f249308018
yana.radeva7 <yana.radeva7@gmail.com>
2022-10-26 19:11:23 +0300

    add ApplicationUser entity configuration

---
+++ ExploreBulgaria.Data/Configurations/ApplicationUserConfiguration.cs
@@ -0,0 +1,44 @@
+﻿using ExploreBulgaria.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using static ExploreBulgaria.Data.Models.Constants.DataConstants.User;
+
+namespace ExploreBulgaria.Data.Configurations
+{
+    public class ApplicationUserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationUser> appUser)
+        {
+            appUser
+                .Property(u => u.UserName)
+                .IsRequired()
+                .HasMaxLength(UserNameMaxLength);
+
+            appUser
+               .Property(u => u.Email)
+               .IsRequired()
+               .HasMaxLength(EmailMaxLength);
+
+            appUser
+                .HasMany(u => u.Claims)
+                .WithOne()
+                .HasForeignKey(c => c.UserId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            appUser
+                .HasMany(u => u.Logins)
+                .WithOne()
+                .HasForeignKey(l => l.UserId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+
+            appUser
+                .HasMany(u => u.Roles)
+                .WithOne()
+                .HasForeignKey(r => r.UserId)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+    }
+}
