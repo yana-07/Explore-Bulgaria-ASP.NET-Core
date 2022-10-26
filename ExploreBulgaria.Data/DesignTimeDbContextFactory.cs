3b5728a2bce2e2fc580341b978c12cafbc7f9a48
yana.radeva7 <yana.radeva7@gmail.com>
2022-10-26 19:24:15 +0300

    utilize design-time factory for ApplicationDbContext creation

---
+++ ExploreBulgaria.Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace ExploreBulgaria.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    {
+        public ApplicationDbContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+            builder.UseSqlServer(connectionString, x => x.UseNetTopologySuite());
+
+            return new ApplicationDbContext(builder.Options);   
+        }
+    }
+}
