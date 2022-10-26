879cc51553ea8a1ef611cc53cd0ebb2f6a0d9a84
yana.radeva7 <yana.radeva7@gmail.com>
2022-10-26 19:04:13 +0300

    extend ApplicationUser entity

--- ExploreBulgaria.Data.Models/ApplicationUser.cs
+++ ExploreBulgaria.Data.Models/ApplicationUser.cs
@@ -1,5 +1,8 @@
 ﻿using ExploreBulgaria.Data.Common.Models;
 using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using static ExploreBulgaria.Data.Models.Constants.DataConstants;
 
 namespace ExploreBulgaria.Data.Models
 {
@@ -11,7 +14,19 @@ namespace ExploreBulgaria.Data.Models
             this.Roles = new HashSet<IdentityUserRole<string>>();
             this.Claims = new HashSet<IdentityUserClaim<string>>();
             this.Logins = new HashSet<IdentityUserLogin<string>>();
+            this.CreatedAttractions = new HashSet<Attraction>();
+            this.VisitedAttractions = new HashSet<AttractionUserVisited>();
+            this.WantToVisitAttractions = new HashSet<AttractionUserWantToVisit>();
+            this.AddedToFavoritesAttractions = new HashSet<AttractionUserAddedToFavorites>();
         }
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        [MaxLength(NameMaxLength)]
+        public string LastName { get; set; } = null!;
 
         public DateTime CreatedOn { get; set; }
 
@@ -26,5 +41,13 @@ namespace ExploreBulgaria.Data.Models
         public virtual ICollection<IdentityUserClaim<string>> Claims { get; set; }
 
         public virtual ICollection<IdentityUserLogin<string>> Logins { get; set; }
+
+        public virtual ICollection<Attraction> CreatedAttractions { get; set; }
+
+        public virtual ICollection<AttractionUserVisited> VisitedAttractions { get; set; }
+
+        public virtual ICollection<AttractionUserWantToVisit> WantToVisitAttractions { get; set; }
+
+        public virtual ICollection<AttractionUserAddedToFavorites> AddedToFavoritesAttractions { get; set; }
     }
 }
