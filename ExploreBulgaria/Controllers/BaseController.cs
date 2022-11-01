aabd95b963473f39d7d112de4663986ef3eae0e6
yana.radeva7 <yana.radeva7@gmail.com>
2022-11-01 10:20:03 +0200

    create base controller to be accessed by authorized users only

---
+++ ExploreBulgaria/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExploreBulgaria.Web.Controllers
+{
+    [Authorize]
+    public class BaseController : Controller
+    {
+    }
+}
