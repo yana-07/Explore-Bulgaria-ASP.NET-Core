8f6b484d69a220bae9e896789276c50625f8dc6c
yana.radeva7 <yana.radeva7@gmail.com>
2022-11-02 17:31:22 +0200

    add users service

---
+++ ExploreBulgaria.Services.Data/IUsersService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace ExploreBulgaria.Web.ViewModels.Users;
+
+public interface IUsersService
+{
+    Task<IdentityResult> SignUpAsync(RegisterViewModel model);
+
+    Task<SignInResult> SignInAsync(LoginViewModel model);
+
+    Task SignOutAsync();
+}
---
+++ ExploreBulgaria.Services.Data/UsersService.cs
@@ -0,0 +1,70 @@
+﻿using ExploreBulgaria.Data.Models;
+using ExploreBulgaria.Web.ViewModels.Users;
+using Microsoft.AspNetCore.Identity;
+
+namespace ExploreBulgaria.Services.Data
+{
+    public class UsersService : IUsersService
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public UsersService(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        public Task<SignInResult> SignInAsync(LoginViewModel model)
+        {
+            var user = userManager.FindByEmailAsync(model.Email).GetAwaiter().GetResult();
+
+            return signInManager.PasswordSignInAsync(user, model.Password, false, false);
+        }
+
+        public Task<IdentityResult> SignUpAsync(RegisterViewModel model)
+        {
+            var emailExists = userManager.FindByEmailAsync(model.Email).GetAwaiter().GetResult() != null;
+            var usernameExists = userManager.FindByNameAsync(model.UserName).GetAwaiter().GetResult() != null;
+
+            if (emailExists)
+            {
+                var error = new IdentityError
+                {
+                    Description = "An user with this email address already exists."
+                };
+
+                return Task.FromResult(IdentityResult.Failed(error));
+            }
+
+            if (usernameExists)
+            {
+                var error = new IdentityError
+                {
+                    Description = "An user with this username already exists."
+                };
+
+                return Task.FromResult(IdentityResult.Failed(error));
+            }
+
+            var user = new ApplicationUser
+            {
+                FirstName = model.FirstName,
+                LastName = model.LastName,
+                UserName = model.UserName,
+                Email = model.Email               
+            };
+
+            // TODO: conditionally add user to LocalGuide Role 
+
+            return userManager.CreateAsync(user, model.Password);
+        }
+
+        public async Task SignOutAsync()
+        {
+            await this.signInManager.SignOutAsync();
+        }
+    }
+}
