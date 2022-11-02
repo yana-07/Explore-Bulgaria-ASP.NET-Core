305064efc66bf31e111bca048cba48277a8faf67
yana.radeva7 <yana.radeva7@gmail.com>
2022-11-02 17:30:06 +0200

    add login and register view models

---
+++ ExploreBulgaria.ViewModels/Users/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExploreBulgaria.Web.ViewModels.Users
+{
+    public class LoginViewModel
+    {
+        [Required]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+    }
+}
---
+++ ExploreBulgaria.ViewModels/Users/RegisterViewModel.cs
@@ -0,0 +1,42 @@
+﻿using ExploreBulgaria.Data.Models;
+using ExploreBulgaria.Services.Mapping;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using static ExploreBulgaria.Data.Common.Constants.DataConstants;
+using static ExploreBulgaria.Data.Common.Constants.DataConstants.User;
+
+namespace ExploreBulgaria.Web.ViewModels.Users
+{
+    public class RegisterViewModel : IMapFrom<ApplicationUser>
+    {
+        [Required]
+        [StringLength(NameMaxLength, MinimumLength = NameMinLength)]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; } = null!;
+
+        [Required]
+        [StringLength(NameMaxLength, MinimumLength = NameMinLength)]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; } = null!;
+
+        [Required]
+        [StringLength(UserNameMaxLength, MinimumLength = UserNameMinLength)]
+        [DisplayName("Username")]
+        public string UserName { get; set; } = null!;
+
+        [Required]
+        [EmailAddress]
+        [StringLength(EmailMaxLength, MinimumLength = EmailMinLength)]
+        public string Email { get; set; } = null!;
+
+        [Required]
+        [MinLength(PasswordMinLength)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = null!;
+
+        [Compare(nameof(Password))]
+        [DataType(DataType.Password)]
+        [DisplayName("Confirm Password")]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
