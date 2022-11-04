689b68444ef1b7c68006e8471bb972d6453b8fa2
yana.radeva7 <yana.radeva7@gmail.com>
2022-11-04 13:35:21 +0200

    use custom status code pages

--- ExploreBulgaria/Controllers/HomeController.cs
+++ ExploreBulgaria/Controllers/HomeController.cs
@@ -28,5 +28,15 @@ namespace ExploreBulgaria.Web.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult StatusCodeError(int statusCode)
+        {
+            if (statusCode == 404)
+            {
+                return View("404");
+            }
+
+            return View();
+        }
     }
 }
--- ExploreBulgaria/Program.cs
+++ ExploreBulgaria/Program.cs
@@ -47,12 +47,14 @@ using (var serviceScope = app.Services.CreateScope())
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseMigrationsEndPoint();
+    //app.UseMigrationsEndPoint();
+    app.UseDeveloperExceptionPage();
+    app.UseStatusCodePagesWithRedirects("/Home/StatusCodeError?statusCode={0}");
 }
 else
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseStatusCodePagesWithRedirects("/Home/StatusCodeError?statusCode={0}");
     app.UseHsts();
 }
 
