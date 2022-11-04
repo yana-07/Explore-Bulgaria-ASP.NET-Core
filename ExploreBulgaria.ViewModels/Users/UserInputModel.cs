7eb1c090a41c18f48e06fb82acfacf9ffbed6745
yana.radeva7 <yana.radeva7@gmail.com>
2022-11-04 12:27:20 +0200

    implement check username and email availability on register with ajax

---
+++ ExploreBulgaria.ViewModels/Users/UserInputModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ExploreBulgaria.Web.ViewModels.Users
+{
+    public class UserInputModel
+    {
+        public string? UserName { get; set; }
+
+        public string? Email { get; set; }
+    }
+}
---
+++ ExploreBulgaria/Controllers/GetEmailUserNameController.cs
@@ -0,0 +1,31 @@
+﻿using ExploreBulgaria.Data.Models;
+using ExploreBulgaria.Web.ViewModels.Users;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExploreBulgaria.Web.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GetEmailUserNameController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public GetEmailUserNameController(UserManager<ApplicationUser> userManager)
+        {
+            this.userManager = userManager;
+        }
+
+        [HttpPost("username")]
+        public async Task<ActionResult<bool>> UserNameExists(UserInputModel input)
+        {
+            return await userManager.FindByNameAsync(input.UserName) != null;
+        }
+
+        [HttpPost("email")]
+        public async Task<ActionResult<bool>> EmailExists(UserInputModel input)
+        {
+            return await userManager.FindByEmailAsync(input.Email) != null;
+        }
+    }
+}
