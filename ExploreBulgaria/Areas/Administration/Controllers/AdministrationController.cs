80a62a4d253904f36ac98b99bdc3942521e52c72
yana.radeva7 <yana.radeva7@gmail.com>
2022-11-04 09:31:16 +0200

    add administration area

---
+++ ExploreBulgaria/Areas/Administration/Controllers/AdministrationController.cs
@@ -0,0 +1,13 @@
+﻿using ExploreBulgaria.Web.Controllers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static ExploreBulgaria.Common.GlobalConstants;
+
+namespace ExploreBulgaria.Web.Areas.Administration.Controllers
+{
+    [Authorize(Roles = AdministratorRoleName)]
+    [Area("Administration")]
+    public class AdministrationController : BaseController
+    {
+    }
+}
