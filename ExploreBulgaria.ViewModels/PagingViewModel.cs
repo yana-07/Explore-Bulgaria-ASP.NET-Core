380d53b0470c737f64c22c6ac6c6f621d1807feb
yana.radeva7 <yana.radeva7@gmail.com>
2022-11-07 09:19:38 +0200

    add paging partial view

---
+++ ExploreBulgaria.ViewModels/PagingViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace ExploreBulgaria.Web.ViewModels
+{
+    public class PagingViewModel
+    {
+        public int PageNumber { get; set; }
+
+        public bool HasPreviousPage => PageNumber > 1;
+
+        public int PreviousPageNumber => PageNumber - 1;
+
+        public bool HasNextPage => PageNumber < PagesCount;
+
+        public int NextPageNumber => PageNumber + 1;
+
+        public int PagesCount => (int)Math.Ceiling((double)(ItemsCount / ItemsPerPage));
+
+        public int ItemsCount { get; set; }
+
+        public int ItemsPerPage { get; set; }
+    }
+}
