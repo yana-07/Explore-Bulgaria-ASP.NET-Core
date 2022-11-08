c0231c2c31d73e3fc331197c3df957b975c84990
yana.radeva7 <yana.radeva7@gmail.com>
2022-11-08 23:46:09 +0200

    add ClaimsPrincipal extension method to get the current user's Id

---
+++ ExploreBulgaria/Common/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+
+namespace ExploreBulgaria.Web.Common
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static string Id(this ClaimsPrincipal user)
+            => user.FindFirstValue(ClaimTypes.NameIdentifier);     
+    }
+}
