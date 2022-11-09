0345fd7d3e300162f24f1c7f259aa1b5a9f91704
yana.radeva7 <yana.radeva7@gmail.com>
2022-11-09 13:02:19 +0200

    add ClaimsPrincipal extension methods for FirstName, LastName and Email

--- ExploreBulgaria/Common/ClaimsPrincipalExtensions.cs
+++ ExploreBulgaria/Common/ClaimsPrincipalExtensions.cs
@@ -5,6 +5,15 @@ namespace ExploreBulgaria.Web.Common
     public static class ClaimsPrincipalExtensions
     {
         public static string Id(this ClaimsPrincipal user)
-            => user.FindFirstValue(ClaimTypes.NameIdentifier);     
+            => user.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        public static string FirstName(this ClaimsPrincipal user)
+            => user.FindFirstValue(ClaimTypes.GivenName);
+
+        public static string LastName(this ClaimsPrincipal user)
+            => user.FindFirstValue(ClaimTypes.Surname);
+
+        public static string Email(this ClaimsPrincipal user)
+            => user.FindFirstValue(ClaimTypes.Email);
     }
 }
