834c5bfb7aef081291f7619c7f630d502ac074b4
yana.radeva7 <yana.radeva7@gmail.com>
2022-11-17 23:06:57 +0200

    separate script from view

---
+++ ExploreBulgaria/wwwroot/js/leaflet.js
@@ -0,0 +1,15 @@
+﻿const lat = Number(document.getElementById('latitude').value);
+const lng = Number(document.getElementById('longitude').value);
+var description = document.getElementById('description').value;
+const zoom = 13;
+
+var map = L.map('map').setView([lat, lng], zoom);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+L.marker([lat, lng]).addTo(map)
+    .bindPopup(`${description}...`)
+    .openPopup();;
