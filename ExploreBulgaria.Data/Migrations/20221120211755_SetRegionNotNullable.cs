b166722ce558ae2f424a86db1600259fd83181f2
yana.radeva7 <yana.radeva7@gmail.com>
2022-11-20 23:19:51 +0200

    set Region FK required

---
+++ ExploreBulgaria.Data/Migrations/20221120211755_SetRegionNotNullable.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExploreBulgaria.Data.Migrations
+{
+    public partial class SetRegionNotNullable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Attractions_Regions_RegionId",
+                table: "Attractions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RegionId",
+                table: "Attractions",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Attractions_Regions_RegionId",
+                table: "Attractions",
+                column: "RegionId",
+                principalTable: "Regions",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Attractions_Regions_RegionId",
+                table: "Attractions");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RegionId",
+                table: "Attractions",
+                type: "nvarchar(450)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Attractions_Regions_RegionId",
+                table: "Attractions",
+                column: "RegionId",
+                principalTable: "Regions",
+                principalColumn: "Id");
+        }
+    }
+}
