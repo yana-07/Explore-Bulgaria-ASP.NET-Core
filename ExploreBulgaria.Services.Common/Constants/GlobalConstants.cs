a6e58ac8c94280ab64ad88945999eebb0ad0ff88
yana.radeva7 <yana.radeva7@gmail.com>
2022-12-02 17:33:46 +0200

    add admin area constant

--- ExploreBulgaria.Services.Common/Constants/GlobalConstants.cs
+++ ExploreBulgaria.Services.Common/Constants/GlobalConstants.cs
@@ -3,6 +3,8 @@
     public static class GlobalConstants
     {
         public const string AdministratorRoleName = "Administrator";
+        public const string AdministrationAreaName = "Administration";
+
         public const string LocalGuideRoleName = "LocalGuide";
 
         public const string SystemName = "ExploreBulgaria";
