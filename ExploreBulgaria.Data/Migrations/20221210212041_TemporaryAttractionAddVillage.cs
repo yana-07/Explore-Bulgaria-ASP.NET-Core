6a60884b3a95558563270497dae99e9923a31357
yana.radeva7 <yana.radeva7@gmail.com>
2022-12-10 23:21:36 +0200

    add village property to TemporaryAttraction entity model

---
+++ ExploreBulgaria.Data/Migrations/20221210212041_TemporaryAttractionAddVillage.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExploreBulgaria.Data.Migrations
+{
+    public partial class TemporaryAttractionAddVillage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Village",
+                table: "TemporaryAttractions",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Village",
+                table: "TemporaryAttractions");
+        }
+    }
+}
