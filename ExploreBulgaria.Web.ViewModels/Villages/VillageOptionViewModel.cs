0fca53b066347a658fb07446d077360f11effbaa
yana.radeva7 <yana.radeva7@gmail.com>
2022-12-11 17:03:40 +0200

    add VillageOptionViewModel for visualizing of all available villages in the admin area when attraction is being processed by admin

---
+++ ExploreBulgaria.Web.ViewModels/Villages/VillageOptionViewModel.cs
@@ -0,0 +1,10 @@
+﻿using ExploreBulgaria.Data.Models;
+using ExploreBulgaria.Services.Mapping;
+
+namespace ExploreBulgaria.Web.ViewModels.Villages
+{
+    public class VillageOptionViewModel : VillageSelectViewModel, IMapFrom<Village>
+    {
+        public string Id { get; set; } = null!;
+    }
+}
