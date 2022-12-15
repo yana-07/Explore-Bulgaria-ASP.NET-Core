17b58a05008bbaa2b26a96d251b5f976d6d830e6
yana.radeva7 <yana.radeva7@gmail.com>
2022-12-15 17:36:40 +0200

    add notifications property to Visitor entity model

---
+++ ExploreBulgaria.Data/Migrations/20221215084326_AddVisitorNotifications.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ExploreBulgaria.Data.Migrations
+{
+    public partial class AddVisitorNotifications : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Notifications",
+                table: "Visitors",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Notifications",
+                table: "Visitors");
+        }
+    }
+}
