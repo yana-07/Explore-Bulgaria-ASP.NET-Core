069af78f6fa55cc5af0eff8b90f53d8cb5bfe3bc
yana.radeva7 <yana.radeva7@gmail.com>
2022-12-16 22:23:25 +0200

    comment DesignTimeDbContextFactory method - does not support secrets.json configuration file

--- ExploreBulgaria.Data/DesignTimeDbContextFactory.cs
+++ ExploreBulgaria.Data/DesignTimeDbContextFactory.cs
@@ -4,20 +4,20 @@ using Microsoft.Extensions.Configuration;
 
 namespace ExploreBulgaria.Data
 {
-    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
-    {
-        public ApplicationDbContext CreateDbContext(string[] args)
-        {
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .Build();
+    //public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+    //{
+    //    public ApplicationDbContext CreateDbContext(string[] args)
+    //    {
+    //        var configuration = new ConfigurationBuilder()
+    //            .SetBasePath(Directory.GetCurrentDirectory())
+    //            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    //            .Build();
 
-            var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
-            var connectionString = configuration.GetConnectionString("DefaultConnection");
-            builder.UseSqlServer(connectionString, x => x.UseNetTopologySuite());
+    //        var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+    //        var connectionString = configuration.GetConnectionString("DefaultConnection");
+    //        builder.UseSqlServer(connectionString, x => x.UseNetTopologySuite());
 
-            return new ApplicationDbContext(builder.Options);   
-        }
-    }
+    //        return new ApplicationDbContext(builder.Options);
+    //    }
+    //}
 }
