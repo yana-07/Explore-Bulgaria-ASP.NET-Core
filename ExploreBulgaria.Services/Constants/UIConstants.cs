a8a2dbb3c1650a770819b7e3dd67d08c84536d31
yana.radeva7 <yana.radeva7@gmail.com>
2022-12-18 00:14:08 +0200

    additional UI constants

--- ExploreBulgaria.Services/Constants/UIConstants.cs
+++ ExploreBulgaria.Services/Constants/UIConstants.cs
@@ -3,6 +3,7 @@
     public static class UIConstants
     {
         public const string FieldRequired = "Полето {0} е задължително.";
+        public const string FieldIdRequired = "Полето е задължително.";
         public const string FieldLength = "Полето {0} трябва да бъде между {2} и {1} символа.";
         public const string FieldMinLength = "Полето {0} трябва да бъде поне {1} символа.";
         public const string FieldVillageNameRegex = "Полето {0} трябва да започва със \"село\"";
@@ -16,5 +17,7 @@
         public const string DisplayDescription = "Описание";
         public const string DisplayRegion = "Регион";
         public const string DisplayVillage = "Село";
+        public const string DisplayLatitude = "Географска ширина";
+        public const string DisplayLongitude = "Географска дължина";
     }
 }
